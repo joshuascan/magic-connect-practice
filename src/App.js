1fa663d6ecaac6ef375877b28db9ed58978a26a1
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-01-13 09:42:59 -0800

    adds styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
 function App() {
   return (
-    <div className="App">
-      <form>
-        <label>email</label>
-        <input 
-          type="text"
-        />
+    <div className="mt-40 flex justify-center ">
+      <form className="w-[32rem] flex flex-col items-center">
+        <div className="flex justify-center">
+          <label className="font-semibold text-lg text-white mx-4">email</label>
+          <input
+            type="text"
+            className="w-72 rounded-md mx-4 px-2 outline-none"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-40 flex justify-center bg-gray-800 border-gray-700 text-white hover:bg-gray-700 active:bg-gray-500 border rounded-lg font-semibold text-xl mt-8 px-5 py-2.5"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
