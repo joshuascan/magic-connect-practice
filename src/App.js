9616c596e5c80665fe61be05f921118659555ea7
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-01-13 13:03:51 -0800

    adds wallet connect/disconnect functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,97 @@
-function App() {
-  return (
-    <div className="mt-40 flex justify-center ">
-      <form className="w-[32rem] flex flex-col items-center">
-        <div className="flex justify-center">
-          <label className="font-semibold text-lg text-white mx-4">email</label>
-          <input
-            type="text"
-            className="w-72 rounded-md mx-4 px-2 outline-none"
-          />
-        </div>
+import { useState, useEffect } from "react";
+import { Magic } from "magic-sdk";
+import { ConnectExtension } from "@magic-ext/connect";
+import { ethers } from "ethers";
 
-        <button
-          type="submit"
-          className="w-40 flex justify-center bg-gray-800 border-gray-700 text-white hover:bg-gray-700 active:bg-gray-500 border rounded-lg font-semibold text-xl mt-8 px-5 py-2.5"
-        >
-          Submit
-        </button>
-      </form>
+const customNodeOptions = {
+  rpcUrl: "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+  chainId: 1,
+};
+
+const magic = new Magic(process.env.REACT_APP_MAGIC_KEY, {
+  network: customNodeOptions,
+  extensions: [new ConnectExtension()],
+});
+
+const provider = new ethers.providers.Web3Provider(magic.rpcProvider);
+
+function App() {
+  const [account, setAccount] = useState(null);
+  const [walletInfo, setWalletInfo] = useState(null);
+
+  const fetchWallet = async () => {
+    const res = await magic.connect.getWalletInfo();
+    setWalletInfo(res);
+  };
+
+  useEffect(() => {
+    fetchWallet();
+  }, [account]);
+
+  const login = async () => {
+    provider
+      .listAccounts()
+      .then((accounts) => setAccount(accounts[0]))
+      .catch((error) => console.error(error));
+  };
+
+  const showWallet = () => {
+    magic.connect.showWallet().catch((error) => {
+      console.error(error);
+    });
+  };
+
+  const disconnectWallet = async () => {
+    await magic.connect.disconnect().catch((error) => console.error(error));
+    setAccount(null);
+    setWalletInfo(null);
+  };
+
+  return (
+    <div className="mt-32 flex justify-center ">
+      {!account && (
+        <div className="flex flex-col items-center">
+          <h1 className="text-white font-bold text-3xl">
+            Wallet Not Connected
+          </h1>
+          <button
+            onClick={login}
+            className="w-40 flex justify-center bg-gray-800 border-gray-700 text-white hover:bg-gray-700 active:bg-gray-500 border rounded-lg font-semibold text-xl mt-8 px-5 py-2.5"
+          >
+            Sign In
+          </button>
+        </div>
+      )}
+
+      {account && (
+        <div>
+          <div className="flex flex-col items-center mb-4">
+            <h2 className="text-white font-bold text-3xl">Wallet Address:</h2>
+            <h2 className="text-white font-semibold italic text-2xl">
+              {account}
+            </h2>
+            <h2 className="mt-6 text-white font-bold text-3xl">Wallet Type:</h2>
+            <h2 className="text-white font-semibold italic text-2xl">
+              {walletInfo?.walletType}
+            </h2>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <button
+              onClick={showWallet}
+              className="w-52 flex justify-center bg-gray-800 border-gray-700 text-white hover:bg-gray-700 active:bg-gray-500 border rounded-lg font-semibold text-xl mt-8 px-5 py-2.5"
+            >
+              Show Wallet
+            </button>
+            <button
+              onClick={disconnectWallet}
+              className="w-40 flex justify-center bg-red-900 border-gray-700 text-white hover:bg-red-700 active:bg-red-500 border rounded-lg font-semibold text-md mt-8 px-5 py-2.5"
+            >
+              Disconnect
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
